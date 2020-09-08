6de7abfd78ccb970bd87baa151a6ffffee22e06f
wangdahoo <157195705@qq.com>
2020-09-08 17:39:38 +0800

    add: add index.d.ts

---
+++ dist/index.d.ts
@@ -0,0 +1,8 @@
+export default class Storage {
+    static get (key: string): void
+    static set (key: string, value: any): boolean
+    static remove (key: string): void
+    static clear (): void
+    static on (key: string, fn: () => void): void
+    static off (key: string, fn: () => void): void
+}
